d36688fca09d5f52754cb6c419006b31bbee29fc
Evan Lin <evan.if.lin@linecorp.com>
2023-06-10 23:13:51 +0800

    update to original ne

--- pdf.py
+++ pdf.py
@@ -1,12 +1,8 @@
-from langchain.memory import ConversationBufferMemory
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferWindowMemory
 from langchain.document_loaders import PyPDFLoader  # for loading the pdf
 from langchain.embeddings import OpenAIEmbeddings  # for creating embeddings
 from langchain.vectorstores import Chroma  # for the vectorization part
 from langchain.chains import ChatVectorDBChain  # for chatting with the pdf
 from langchain.llms import OpenAI  # the LLM model we'll use (CHatGPT)
-from langchain.chains import ConversationChain
 
 
 pdf_path = "./paper.pdf"
@@ -30,30 +26,3 @@ query = "What is the bitcoin?"
 result = pdf_qa({"question": query, "chat_history": ""})
 print("Answer:")
 print(result["answer"])
-
-
-memory = ConversationBufferWindowMemory(k=1)
-memory.save_context({"input": "Hi"},
-                    {"output": "What's up"})
-memory.save_context({"input": "Not much, just hanging"},
-                    {"output": "Cool"})
-
-
-# llm = ChatOpenAI(temperature=0.0)
-memory = ConversationBufferWindowMemory(k=3)
-conversation = ConversationChain(
-    llm=llm,
-    memory=memory,
-    verbose=False
-)
-
-
-conversation.predict(input="Hi, my name is Andrew")
-conversation.predict(input="What is 1+1?")
-conversation.predict(input="What is my name?")
-print(memory.buffer)
-memory.load_memory_variables({})
-memory = ConversationBufferMemory()
-memory.save_context({"input": "Hi"},
-                    {"output": "What's up"})
-print(memory.buffer)
