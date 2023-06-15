124874c820c57df77f3d22d6d467ec6b82043833
Evan Lin <evan.if.lin@linecorp.com>
2023-06-15 20:53:20 +0800

    feat: delete unnecessary markdown files with OpenAI_Functions tool
    - Add a tool to delete files in OpenAI_Functions.py
    - Use the new tool to delete markdown files in a chat message
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- OpenAI_Functions.py
+++ OpenAI_Functions.py
@@ -2,7 +2,7 @@
 # Make sure langchain to 0.0.200
 # pip install --upgrade --force-reinstall langchain
 
-from langchain.tools import MoveFileTool, format_tool_to_openai_function
+from langchain.tools import MoveFileTool, DeleteFileTool, format_tool_to_openai_function
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
@@ -14,6 +14,16 @@ functions = [format_tool_to_openai_function(t) for t in tools]
 
 message = model.predict_messages(
     [HumanMessage(content='move file foo to bar')], functions=functions)
+
+print(message)
+
+print(message.additional_kwargs['function_call'])
+
+del_tools = [DeleteFileTool()]
+del_functions = [format_tool_to_openai_function(t) for t in del_tools]
+
+message = model.predict_messages(
+    [HumanMessage(content='delete *.md from Document folder')], functions=del_functions)
 
 print(message)
 
