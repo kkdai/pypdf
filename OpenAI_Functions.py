c766edaf8ba9709ce418329e604c5fc6e1d42f1b
Evan Lin <evan.if.lin@linecorp.com>
2023-06-17 11:46:03 +0800

    feat: This commit introduces new features to the codebase, such as a stock tool and a tool for deleting files.: refactor code and add new tools for user input and file manipulation
    - Add a stock tool for checking stock prices
    - Remove unused imports and variables
    - Modify the code to allow user input for question answering
    - Delete unused code for moving files and add a new tool for deleting files
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- OpenAI_Functions.py
+++ OpenAI_Functions.py
@@ -2,29 +2,50 @@
 # Make sure langchain to 0.0.200
 # pip install --upgrade --force-reinstall langchain
 
+import json
 from langchain.tools import MoveFileTool, DeleteFileTool, format_tool_to_openai_function
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+from stock_tool import StockPriceTool
+
 
 model = ChatOpenAI(model="gpt-3.5-turbo-0613")
 
-
-tools = [MoveFileTool()]
+# Prepare openai.functions
+tools = [StockPriceTool()]
 functions = [format_tool_to_openai_function(t) for t in tools]
 
-message = model.predict_messages(
-    [HumanMessage(content='move file foo to bar')], functions=functions)
+while True:
+    try:
+        question = input("Question: ")
+        hm = HumanMessage(content=question)
+        ai_message = model.predict_messages([hm], functions=functions)
+        ai_message.additional_kwargs['function_call']
+        _args = json.loads(
+            ai_message.additional_kwargs['function_call'].get('arguments'))
+        tool_result = tools[0](_args)
 
-print(message)
+        print("Answer: ", tool_result)
+    except KeyboardInterrupt:
+        break
 
-print(message.additional_kwargs['function_call'])
 
-del_tools = [DeleteFileTool()]
-del_functions = [format_tool_to_openai_function(t) for t in del_tools]
+# tools = [MoveFileTool()]
+# functions = [format_tool_to_openai_function(t) for t in tools]
 
-message = model.predict_messages(
-    [HumanMessage(content='delete *.md from Document folder')], functions=del_functions)
+# message = model.predict_messages(
+#     [HumanMessage(content='move file foo to bar')], functions=functions)
 
-print(message)
+# print(message)
 
-print(message.additional_kwargs['function_call'])
+# print(message.additional_kwargs['function_call'])
+
+# del_tools = [DeleteFileTool()]
+# del_functions = [format_tool_to_openai_function(t) for t in del_tools]
+
+# message = model.predict_messages(
+#     [HumanMessage(content='delete *.md from Document folder')], functions=del_functions)
+
+# print(message)
+
+# print(message.additional_kwargs['function_call'])
